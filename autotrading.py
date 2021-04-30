d7f8a4050b0bab50cff005e96e03f8c5c71bd117
gook-ki <52888129+gook-ki@users.noreply.github.com>
2021-04-30 23:35:55 +0900

    Update autotrading.py

--- autotrading.py
+++ autotrading.py
@@ -6,19 +6,19 @@ access = ""
 secret = ""
 
 def get_target_price(ticker, k):
-    """변동성 돌파 전략으로 매수 목표가 조회"""
+
     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)  #시가, 고가, 저가, 종가(당일 현재가), 거래량
-    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
+    target_price = df.iloc[0]['low'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
 def get_target_price2(ticker):
-    """변동성 돌파 전략으로 매수 목표가 조회"""
+
     dft = pyupbit.get_ohlcv(ticker, interval="day", count=1)  #시가, 고가, 저가, 종가(당일 현재가), 거래량
     target_price2 = dft.iloc[0]['high'] * 0.93
     return target_price2
 
 def nowprice(ticker):
-    """변동성 돌파 전략으로 매수 목표가 조회"""
+
     dft = pyupbit.get_ohlcv(ticker, interval="day", count=1)  #시가, 고가, 저가, 종가(당일 현재가), 거래량
     now_price = dft.iloc[0]['close']
     return now_price
@@ -27,13 +27,11 @@ def nowprice(ticker):
 
 
 def get_start_time(ticker):
-    """시작 시간 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
     start_time = df.index[0]
     return start_time
 
 def get_balance(ticker):
-    """잔고 조회"""
     balances = upbit.get_balances()
     for b in balances:
         if b['currency'] == ticker:
@@ -43,14 +41,13 @@ def get_balance(ticker):
                 return 0
 
 def get_current_price(ticker):
-    """현재가 조회"""
     return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
 
-# 로그인
+# 
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 
-# 자동매매 시작
+#  
 while True:
     try:
         now = datetime.datetime.now()
