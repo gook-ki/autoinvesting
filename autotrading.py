19d6d883c250c22b90791865bf65983a022ee486
gook-ki <52888129+gook-ki@users.noreply.github.com>
2021-05-01 01:31:37 +0900

    Update autotrading.py

--- autotrading.py
+++ autotrading.py
@@ -5,23 +5,28 @@ import datetime
 access = ""
 secret = ""
 
-def get_target_price(ticker, k):
 
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)  #시가, 고가, 저가, 종가(당일 현재가), 거래량
+
+def get_target_price(ticker, k):
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
     target_price = df.iloc[0]['low'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
 def get_target_price2(ticker):
-
-    dft = pyupbit.get_ohlcv(ticker, interval="day", count=1)  #시가, 고가, 저가, 종가(당일 현재가), 거래량
+    dft = pyupbit.get_ohlcv(ticker, interval="day", count=1)
     target_price2 = dft.iloc[0]['high'] * 0.93
     return target_price2
 
 def nowprice(ticker):
-
-    dft = pyupbit.get_ohlcv(ticker, interval="day", count=1)  #시가, 고가, 저가, 종가(당일 현재가), 거래량
+    dft = pyupbit.get_ohlcv(ticker, interval="day", count=1)
     now_price = dft.iloc[0]['close']
     return now_price
+def highprice(ticker):
+    dft = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    high_price = dft.iloc[0]['high']
+    return high_price
+
+
 
 
 
@@ -43,11 +48,11 @@ def get_balance(ticker):
 def get_current_price(ticker):
     return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
 
-# 
+#
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 
-#  
+#
 while True:
     try:
         now = datetime.datetime.now()
@@ -59,17 +64,23 @@ while True:
             target_price = get_target_price("KRW-NEO", 0.8)
             current_price = get_current_price("KRW-NEO")
             now_price=nowprice("KRW-NEO")
-            if target_price < current_price:
+            high_price = highprice("KRW-NEO")
+
+            if target_price < current_price and high_price*0.99 < current_price :
                 krw = get_balance("KRW")
                 if krw > 5000:
+
                     upbit.buy_market_order("KRW-NEO", krw*0.9995)
             if now_price < target_price2:
                 NEO = get_balance("NEO")
                 if NEO > 0.05:
+
                     upbit.sell_market_order("KRW-NEO", NEO * 0.9995)
         else:
             NEO = get_balance("NEO")
             if NEO > 0.05:
+                print("hello")
+
                 upbit.sell_market_order("KRW-NEO", NEO*0.9995)
         time.sleep(1)
     except Exception as e:
